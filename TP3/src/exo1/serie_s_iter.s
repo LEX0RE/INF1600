1a1b086435d1e279aa55ce90242f669f6ec4148f
Dariadanciu <ioana-daria.danciu@polymtl.ca>
2021-03-17 17:10:17 -0400

    question 1

--- TP3/src/exo1/serie_s_iter.s
+++ TP3/src/exo1/serie_s_iter.s
@@ -3,8 +3,30 @@
 serie_s_iter:
    pushl  %ebp
    movl   %esp, %ebp
+   pushl %ebx
    
-   # completer
+   movl 8(%ebp), %edx
+
+   cmpl $0, %edx
+   jz N0
+
+   cmpl $1, %edx
+   jz N1
+
+   movl $2, %eax        #c
+   movl $2, %ebx        #i
+   movl $1, %edx        #p
+
+for:
+   cmpl 8(%ebp), %ebx
+   jg Retour
+
+   pushl %eax
+   addl $1, %eax
+   addl %edx, %eax
+   popl %edx
+   inc %ebx
+   loop for
    
 N0:
    movl   $1, %eax
--- TP3/src/exo1/serie_s_rec.s
+++ TP3/src/exo1/serie_s_rec.s
@@ -3,8 +3,30 @@
 serie_s_rec:
    pushl  %ebp
    movl   %esp, %ebp
+   pushl %ebx
    
-   # completer
+   movl 8(%ebp), %edx
+   movl $1, %ebx
+
+   cmpl $0, %edx
+   jz N0
+
+   cmpl $1, %edx
+   jz N1
+
+	decl %edx
+	pushl %edx
+	call serie_s_rec
+   addl $4, %esp
+   addl %eax, %ebx
+   movl 8(%ebp), %edx
+   subl $2, %edx
+   pushl %edx
+   call serie_s_rec
+   addl $4, %esp
+   addl %eax, %ebx
+   movl %ebx, %eax
+   jmp Retour
    
 N0:
    movl   $1, %eax
@@ -15,6 +37,7 @@ N1:
    jmp    Retour   
    
 Retour:   
+   pop %ebx
    leave
    ret
 
