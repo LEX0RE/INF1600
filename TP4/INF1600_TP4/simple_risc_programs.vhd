ac1ebbb266f2f091ae667bc8621e85ececc42a09
Dariadanciu <ioana-daria.danciu@polymtl.ca>
2021-04-05 12:38:32 -0400

    Fin du TP4

--- TP4/INF1600_TP4/simple_risc_programs.vhd
+++ TP4/INF1600_TP4/simple_risc_programs.vhd
@@ -24,40 +24,27 @@ package simple_risc_programs is
    );
 
    constant program_1 : inst_mem_type := (
-      x"A000006", -- r0    <- +6           - addr x06    nb d'itérations (%ecx)
-      x"A010001", -- r1    <- +1           - addr x00    constante 1
-      x"A020000", -- r2    <- +0           - addr x00    %esp
-      x"A030001", -- r3    <- +1           - addr x00    S(n)
-      x"A040002", -- r4    <- +2           - addr x00    S(n+1)
-      x"9000203", -- M[r2] <- r3           - addr x07
-      x"1000001", -- r0    <- r0 - r1      - addr x05
-      x"C020007", -- jnz 0x07              - addr x0C    modifier pour fin
-      x"0020201", -- r2    <- r2 + r1      - addr x02
-
-      x"9000204", -- M[r2] <- r4           - addr x07
-      x"1000001", -- r0    <- r0 - r1      - addr x05
-      x"C020007", -- jnz 0x07              - addr x0C    modifier pour fin
-
-      x"7050300", -- r5    <- r3           - addr x05
-      x"7030400", -- r3    <- r4           - addr x05
-      x"7040500", -- r4    <- r5           - addr x05
-
-
-
-
-
-
-      x"A010002", -- r1    <- +2           - addr x01
-      x"9000106", -- M[..] <- r0           - addr x07
-      x"9000106", -- M[..] <- r1           - addr x07
-      x"0010100", -- r1    <- r1 + r0      - addr x09
-      x"0010100", -- r1    <- r1 + r0      - addr x09
-      x"9000106", -- M[..] <- r1           - addr x07
-      x"C020007", -- jnz 0x07              - addr x0C
-
-   
-
-
+      x"A000006", -- r0    <- +6           - addr x00    nb d'itérations (%ecx)
+      x"A010001", -- r1    <- +1           - addr x01    constante 1
+      x"A020000", -- r2    <- +0           - addr x02    %esp
+      x"A030001", -- r3    <- +1           - addr x03    S(n)
+      x"A040002", -- r4    <- +2           - addr x04    S(n+1)
+      x"9000203", -- M[r2] <- r3           - addr x05
+      x"1000001", -- r0    <- r0 - r1      - addr x06
+      x"C010015", -- jz 0x15               - addr x07   
+      x"0020201", -- r2    <- r2 + r1      - addr x08
+      x"9000204", -- M[r2] <- r4           - addr x09
+      x"1000001", -- r0    <- r0 - r1      - addr x0A
+      x"C010015", -- jz 0x15               - addr x0B    
+      x"7050300", -- r5    <- r3           - addr x0C
+      x"7030400", -- r3    <- r4           - addr x0D
+      x"7040500", -- r4    <- r5           - addr x0E
+      x"0040304", -- r4    <- r3 + r4      - addr x0F
+      x"0040401", -- r4    <- r4 + r1      - addr x10
+      x"0020201", -- r2    <- r2 + r1      - addr x11
+      x"9000204", -- M[r2] <- r4           - addr x12
+      x"1000001", -- r0    <- r0 - r1      - addr x13
+      x"C02000C", -- jnz 0x0C              - addr x14
 
       others => (others => '1')
    );
